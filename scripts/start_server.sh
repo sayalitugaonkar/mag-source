bae99a1901cec55a523069d6322fe9c7233d5ed9
Sayali Tugaonkar <sayalitugaonkar@gmail.com>
2024-03-30 18:16:50 +0530

    update-scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 chmod 777 -R /var/www/html/
+cd /var/www/
+git clone https://github.com/magento/magento2-sample-data.git
+ls /magento2-sample-data/
 cd /var/www/html/
+php -f /var/www//magento2-sample-data/dev/tools/build-sample-data.php -- --ce-source="/var/www/html/"
 bin/magento setup:upgrade
 bin/magento setup:static-content:deploy -f
 service nginx restart
