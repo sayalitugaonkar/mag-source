f47e955b97d8cbf51566e54b59c5c22ac3c556c1
Sayali Tugaonkar <sayalitugaonkar@gmail.com>
2024-03-30 19:09:11 +0530

    update-scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 chmod 777 -R /var/www/html/
+chown -R test-user:clp /var/www/html/
 cd /var/www/
 git clone https://github.com/magento/magento2-sample-data.git
 ls /magento2-sample-data/
@@ -8,6 +9,8 @@ bin/magento setup:upgrade
 bin/magento setup:di:compile
 php -f /var/www//magento2-sample-data/dev/tools/build-sample-data.php -- --ce-source="/var/www/html/"
 bin/magento setup:static-content:deploy -f
+chmod 777 -R /var/www/html/
+chown -R test-user:clp /var/www/html/
 service nginx restart
 service php8.2-fpm restart
 
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 service nginx stop
 service php8.2-fpm stop
+rm -rf /var/www/html/
+mkdir -p /var/www/html/
+chmod 777 -R /var/www/html/
