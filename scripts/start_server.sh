156e1e7f41144102d6cf4dd6a5cee756c6132167
Sayali Tugaonkar <sayalitugaonkar@gmail.com>
2024-03-30 17:52:02 +0530

    update-scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 chmod 777 -R /var/www/html/
+cd /var/www/html/
+bin/magento setup:upgrade
+bin/magento setup:static-content:deploy -f
 service nginx restart
 service php8.2-fpm restart
 
